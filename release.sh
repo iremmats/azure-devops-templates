7d2ab2897fd2b8716f26ed84775d19564556c8f5
Mats Iremark <mats@iremark.se>
2019-11-22 18:00:34 +0100

    Adding release script

---
+++ release.sh
@@ -0,0 +1,42 @@
+
+echo "VERIFYING PERSONAL ACCESS TOKEN"
+    if [[ -z "$ACCESS_TOKEN_SECRET" ]]; then
+        echo "Please set env var ACCESS_TOKEN_SECRET for git host: $GIT_HOST"
+        exit 1
+    fi
+
+cd "$HOME"
+
+# Remove http(s):// protocol from URL so we can insert PA token
+repo_url=$GIT_CONFIGURATION_REPO
+repo_url="${repo_url#http://}"
+repo_url="${repo_url#https://}"
+
+echo "GIT CLONE: https://$ACCESS_TOKEN_SECRET@$repo_url"
+git clone "https://$ACCESS_TOKEN_SECRET@$repo_url"
+
+repo=${repo_url##*/}
+repo_name=${repo%.*}
+
+cd "$repo_name"
+echo "GIT PULL ORIGIN MASTER"
+git pull origin master
+
+cd $PATH_IN_GIT_CONFIGURATION_REPO
+cd $RELEASE_ENVIRONMENTNAME
+
+sed -i "s/docker_image_tag.*/docker_image_tag:'$BUILD_BUILDID',/g" params.libsonnet
+
+git config user.email "mats@iremark.se"
+git config user.name "Automated Azure Devops Account"
+
+echo "GIT COMMIT"
+git add params.libsonnet
+git commit -m "$PATH_IN_GIT_CONFIGURATION_REPO - $RELEASE_ENVIRONMENTNAME - $BUILD_BUILDID"
+
+echo "GIT PUSH"
+echo "GIT PUSH: https://$ACCESS_TOKEN_SECRET@$repo_url"
+git push "https://$ACCESS_TOKEN_SECRET@$repo_url"
+retVal=$? && [ $retVal -ne 0 ] && exit $retVal
+echo "GIT STATUS"
+git status
